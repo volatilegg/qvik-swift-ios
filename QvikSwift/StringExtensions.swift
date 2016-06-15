49d5b8d3aca195ef365e14c8f7069aba442339e1
Kosti Jokinen <kosti.jokinen@qvik.fi>
2016-06-15 11:05:31 +0300

    Added the UIKit import required by String.boundingRectWithFont()

--- QvikSwift/StringExtensions.swift
+++ QvikSwift/StringExtensions.swift
@@ -20,7 +20,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-import Foundation
+import UIKit
 
 /// Extensions to the String class.
 extension String {
